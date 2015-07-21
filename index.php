92e353208cd216c12113ed8c01e6f7f5924c9633
Ian De La Cruz <ridlc10@gmail.com>
2015-07-21 17:11:12 +0800

    Added services and contact us

--- index.php
+++ index.php
@@ -4,163 +4,47 @@
 		<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title></title>
-		<style type="text/css">
-			body {
-				margin: 0;
-				height: 1200px;
-			}
-			nav {
-				position: fixed;
-				top: 0;
-				background-color: #CCCCCC;
-				width: 100%;
-				height: 50px;
-			}
-			header {
-				display: block;
-				height: 200px;
-				background-image: url('http://placehold.it/940x580');
-			}
-			ul {
-				list-style-type: none;
-				margin: 0;
-				padding: 0;
-				position: absolute;
-
-			}
-			li {
-				display: inline-block;
-			}
-
-			/*Style 'show menu' label button and hide it by default*/
-			.show-menu {
-				font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-				text-decoration: none;
-				color: #fff;
-				background: #19c589;
-				text-align: center;
-				padding: 10px 0;
-				display: none;
-				width: 50%;
-			}
-
-			/*Hide checkbox*/
-			input[type=checkbox]{
-			    display: none;
-			}
-
-			/*Show menu when invisible checkbox is checked*/
-			input[type=checkbox]:checked ~ #navigation{
-			    display: block;
-			}
-
-
-			@media screen and (max-width : 760px){
-				/*Make dropdown links appear inline*/
-				ul {
-					position: static;
-					display: none;
-				}
-				/*Create vertical spacing*/
-				li {
-					margin-bottom: 1px;
-				}
-				/*Make all menu links full width*/
-				ul li, li a {
-					width: 100%;
-					text-align: center;
-				}
-				/*Display 'show menu' link*/
-				.show-menu {
-					display:block;
-					float: right;
-				}
-			}
-		</style>
+		<title>Index</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
 		<header id="header">
 			<nav id="nav_tag">
-				<label for="show-menu" class="show-menu">Show Menu</label>
+				<label for="show-menu" class="show-menu">Menu</label>
 				<input type="checkbox" id="show-menu" role="button">
-				<!-- <ul id="navigation">
-					<li>Link One</li>
-					<li>Link Two</li>
-				</ul> -->
 				<ul id="navigation">
-					<li><a href="#">Home</a></li>
-					<li>
-						<a href="#">About ￬</a>
-					</li>
-					<li>
-						<a href="#">Portfolio ￬</a>
-					</li>
-					<li><a href="#">News</a></li>
-					<li><a href="#">Contact</a></li>
+					<li><a href="services.php">Services</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Portfolio</a></li>
+					<li><a href="#">About us</a></li>
+					<li><a href="contactus.php">Contact us</a></li>
 				</ul>
 			</nav>
+			<div id="head_content">
+
+			</div>
 		</header>
 		<main id="main">
+			<div class="content_div" id="what_we_offer">
+				<h1>WHAT WE OFFER</h1>
+				<div class="flex-container">
+					<div class="circle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p></div>
+					<div class="circle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p></div>
+					<div class="circle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p></div>
+				</div>
+			</div>
+			<div class="content_div" id="sponsors_div">
+				<h1>Sponsors</h1>
+				<div class="flex-container">
+					<div class="circle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p></div>
+					<div class="circle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p></div>
+					<div class="circle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p></div>
+				</div>
+			</div>
 		</main>
 		<footer>
-			<script type="text/javascript">
-				var heading = document.getElementById("header");
-				var main = document.getElementById("main");
-				var nav_tag = document.getElementById("nav_tag");
-				var ul = document.getElementById("navigation");
-				var logo = document.createElement("li");
-				logo.innerHTML = "<img src='http://placehold.it/20x20'>";
-				var style = window.getComputedStyle(heading, null).getPropertyValue("height");
-
-				var par = document.createElement("p")
-				par.textContent = "Hello World";
-				var mql = window.matchMedia("screen and (min-width:48em)");
-				
-				var append = function(mql){
-					if(!mql.matches){
-						heading.style.height = '50px';
-						nav_tag.insertBefore(logo, nav_tag.firstChild);
-						main.appendChild(par);
-					} else {
-						heading.style.height = '200px';
-						if(nav_tag.contains(logo) || main.contains(par)){
-							nav_tag.removeChild(nav_tag.firstChild);
-							main.removeChild(par);
-						}
-					}
-				}
-
-				window.onload = function(){
-					append(mql);
-				}
-
-				// On Window Resize
-				// If width < query -> set nav height, add logo, append special div
-				// If width > query -> set header height to 200 (original),
-				//						if logo and div exists, remove them
-				window.onresize = function(){
-					console.log("resize");
-					append(mql);
-				}
-
-				window.onscroll = function(){
-					console.log("scroll");
-					var rectObject = heading.getBoundingClientRect();
-					//console.log(rectObject.bottom);
-					if(rectObject.bottom <= 50){
-						if(!ul.contains(logo)){
-							ul.insertBefore(logo, ul.firstChild);
-						}
-					} else {
-						if(ul.contains(logo)){
-							ul.removeChild(ul.firstChild);
-						}
-					}
-				}	
-
-				
-			</script>
+			<div>SOMETHING GOES HERE</div>
+			<script type="text/javascript" src="js/main.js"></script>
 		</footer>
 	</body>
 </html>
--- js/main.js
+++ js/main.js
@@ -5,18 +5,46 @@ var main = document.getElementById("main");
 var nav_tag = document.getElementById("nav_tag");
 var ul = document.getElementById("navigation");
 var logo = document.createElement("a");
-logo.innerHTML = "<img src='http://placehold.it/20x20'>";
+logo.innerHTML = "<a href='index.html'><img src='http://placehold.it/20x20'></a>";
 logo.style.position="fixed";
 logo.style.top="20px";
 logo.style.left="20px";
 
 var style = window.getComputedStyle(heading, null).getPropertyValue("height");
 
-var par = document.createElement("p")
-par.textContent = "Hello World";
+var par = document.createElement("p");
+// par.textContent = "Hello World";
+par.setAttribute("class", "dynamic_div");
+
+var head_content = document.getElementById("head_content");
+var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+var title = document.title;
+console.log(title);
+switch( title ){
+	case "Index":
+		par.textContent = 'INDEX ' + text;
+		break;
+	case "Services":
+		par.textContent = 'SERVICES ' + text;
+		break;
+	// case "Index":
+	// 	head_content.innerHTML = 'INDEX ' + text;
+	// 	break;
+	// case "Index":
+	// 	head_content.innerHTML = 'INDEX ' + text;
+	// 	break;
+	// case "Index":
+	// 	head_content.innerHTML = 'INDEX ' + text;
+	// 	break;
+	// case "Index":
+	// 	head_content.innerHTML = 'INDEX ' + text;
+	// 	break;
+}
+
 var mql = window.matchMedia("screen and (min-width:48em)");
 
-
+console.log(head_content.parentNode.nodeName);
 /*
  *	Append Function
  *	@param mql Media Query List
@@ -24,20 +52,28 @@ var mql = window.matchMedia("screen and (min-width:48em)");
  */
 var append = function(mql){
 	if(!mql.matches){
-		heading.style.height = '100px';
+		heading.style.height = '80px';
 		nav_tag.insertBefore(logo, nav_tag.firstChild);
-		main.appendChild(par);
+		heading.contains(head_content) ? heading.removeChild(head_content) : console.log("null");
+		main.insertBefore(par,main.firstChild);
 	} else {
-		heading.style.height = '700px';
+		title =="Contact Us" ? heading.style.height = '80px' : heading.style.height = '700px';
 		if(nav_tag.contains(logo) || main.contains(par)){
 			nav_tag.removeChild(nav_tag.firstChild);
-			main.removeChild(par);
+			main.contains(par) ? main.removeChild(par) : console.log("null");
 		}
+		heading.appendChild(head_content);
 	}
 }
 
 window.onload = function(){
 	append(mql);
+	var rectObject = heading.getBoundingClientRect();
+	if(rectObject.bottom == 700){
+		nav_tag.style["box-shadow"]="none";
+	} else {
+		title =="Contact Us" ? nav_tag.style["box-shadow"]="none" : nav_tag.style["box-shadow"]= "0 3px 8px rgba(0,0,0,.25)";
+	}
 }
 
 /*	On Window Resize
@@ -47,25 +83,56 @@ window.onload = function(){
  */
 window.onresize = function(){
 	console.log("resize");
-	append(mql);
+	append(mql); 
 }
 
 window.onscroll = function(){
 	console.log("scroll");
 	var rectObject = heading.getBoundingClientRect();
-	
+	console.log(rectObject.bottom);
 	/*
 	*
 	*	rectObject.bottom <= value
 	* 	value → amount of box already hidden as calculated by getBoundingClientRect()
 	*	
 	*/
-	if(rectObject.bottom <= 100){
+	// if(rectObject.bottom <= 500){
+	// 	nav_tag.style["box-shadow"]= "0 3px 8px rgba(0,0,0,.25)";
+	// 	if(!nav_tag.contains(logo)){
+	// 		nav_tag.insertBefore(logo, nav_tag.firstChild);
+	// 		if(mql.matches){
+	// 			ul.style.float="right";
+	// 		} else {
+	// 			console.log("HERE");
+	// 		}
+	// 	}
+	// } else if(rectObject.bottom == 80 && title =="Contact Us") {
+	// 	console.log("HI");
+	// } else {
+	// 	nav_tag.style["box-shadow"]= "none";
+	// 	if(nav_tag.contains(logo)){
+	// 		nav_tag.removeChild(nav_tag.firstChild);
+	// 		ul.style.float="none";
+	// 	}
+	// }
+	if(rectObject.bottom == 80 && title =="Contact Us") {
+		nav_tag.style["box-shadow"]= "none";
+		if(nav_tag.contains(logo)){
+			nav_tag.removeChild(nav_tag.firstChild);
+			ul.style.float="none";
+		}
+	} else if( rectObject.bottom <= 500){
+		nav_tag.style["box-shadow"]= "0 3px 8px rgba(0,0,0,.25)";
 		if(!nav_tag.contains(logo)){
 			nav_tag.insertBefore(logo, nav_tag.firstChild);
-			ul.style.float="right";
+			if(mql.matches){
+				ul.style.float="right";
+			} else {
+				console.log("HERE");
+			}
 		}
 	} else {
+		nav_tag.style["box-shadow"]= "none";
 		if(nav_tag.contains(logo)){
 			nav_tag.removeChild(nav_tag.firstChild);
 			ul.style.float="none";

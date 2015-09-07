66ae82974cea235bda1d6ed55a326474ee69b479
Chi Señires <chi.senires@gmail.com>
2015-09-07 20:53:15 +0800

    Added Google Analytics script

--- aboutus.php
+++ aboutus.php
@@ -299,6 +299,14 @@
 			<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
 			<script type="text/javascript" src="js/jquery.transit.min.js"></script>
 			<script type="text/javascript" src="js/main.min.js"></script>
+			<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			  ga('create', 'UA-46284584-11', 'auto');
+			  ga('send', 'pageview');
+			</script>
 		</footer>
 	</body>
 </html>
